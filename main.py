b7d497f50020d3d79ac6fa83a7592e94bf932159
danbugs <danilochiarlone@gmail.com>
2023-05-03 16:08:14 -0700

    several improvements
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import copy
+import sys
 
 def get_curr_seeding(average_win_rates, should_print=True):
     player_standing = []
@@ -23,89 +25,146 @@ def get_curr_seeding(average_win_rates, should_print=True):
 
     return player_standing
 
-# Read URLs from file
-with open('players.txt', 'r') as f:
-    urls = [line.strip() for line in f]
+def is_seeding_stable(curr_players, prev_players, threshold):
+    curr_dict = {curr_players[i]: i+1 for i in range(len(curr_players))}
+    prev_dict = {prev_players[i]: i+1 for i in range(len(prev_players))}
+    curr_seedings = [curr_dict[player] for player in curr_players]
+    prev_seedings = [prev_dict[player] for player in curr_players]
+    differences = [abs(curr_seedings[i] - prev_seedings[i]) for i in range(len(curr_seedings))]
+    mad = sum(differences) / len(differences)
+    return mad < threshold
 
-# Set up the webdriver
-driver = webdriver.Chrome()
+# Get command-line arg to decide whether to web-scrape
+if len(sys.argv) > 1 and sys.argv[1] == 'scrape':
+    scrape = True
+else:
+    scrape = False
 
-average_win_rates = {}
-player_records = {}
+if scrape:
+    # Read URLs from file
+    with open('players.txt', 'r') as f:
+        urls = [line.strip() for line in f]
 
-print("\n>>> Calculating...")
-# Loop over URLs
-for url in urls:
-    driver.get(url)
+    # Set up the webdriver
+    driver = webdriver.Chrome()
 
-    # Wait for the page to load
-    wait = WebDriverWait(driver, 60)
-    elements = wait.until(EC.presence_of_all_elements_located(
-        (By.CLASS_NAME, 'css-k9b79k')))
+    average_win_rates = {}
+    player_records = {}
+    overall_win_rates = {}
 
-    # Get the second search field with class 'Search_textInput__2opNd'
-    search_field = driver.find_elements(
-        By.CLASS_NAME,  'Search_textInput__2opNd')[1]
+    print("\n>>> Calculating...")
+    # Loop over URLs
+    for url in urls:
+        driver.get(url)
 
-    # Read player names from file
-    with open('search.txt', 'r') as f:
-        players = [line.strip() for line in f]
+        # Wait for the page to load
+        wait = WebDriverWait(driver, 60)
+        elements = wait.until(EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, 'css-k9b79k')))
 
-    # Initialize dictionary to store win rates for each player
-    win_rates = {}
+        # Get the second search field with class 'Search_textInput__2opNd'
+        search_field = driver.find_elements(
+            By.CLASS_NAME,  'Search_textInput__2opNd')[1]
 
-    # Loop over players
-    for player in players:
-        # Input player name into search field
-        search_field.clear()
-        search_field.send_keys(player)
-        search_field.send_keys(Keys.RETURN)
+        # Read player names from file
+        with open('search.txt', 'r') as f:
+            players = [line.strip() for line in f]
 
-    # Get the page source
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+        # Initialize dictionary to store win rates for each player
+        win_rates = {}
 
-    # Find the win rate data
-    rows = soup.find_all('div', class_='css-k9b79k')
-    for row in rows:
-        player_name = row.find('div', class_='css-k6n90a')
-        win_rate = row.find('div', class_='css-he2tnk')
-        if player_name and win_rate and player_name.text in players:
-            # Remove % sign from win rate and convert to float
-            win_rate_val = float(win_rate.text.replace('%', ''))
-            # Add win rate to dictionary for player
-            if player_name.text in win_rates:
-                win_rates[player_name.text] += win_rate_val
-            else:
-                win_rates[player_name.text] = win_rate_val
+        # Click button with ID rcc-confirm-button, if it exists
+        try:
+            confirm_button = driver.find_element(By.ID, 'rcc-confirm-button')
+            confirm_button.click()
+        except:
+            pass
+        
+        # Loop over players
+        for player in players:
+            # Input player name into search field
+            search_field.clear()
+            search_field.send_keys(player)
+            search_field.send_keys(Keys.RETURN)
 
-    player_name = soup.find('div', class_='css-1ob8mn4').text
-    player_records[player_name] = win_rates
+        # Find last `select` in the page, and select the third option
+        select = driver.find_elements(By.TAG_NAME, 'select')[-1]
+        select.click()
+        options = select.find_elements(By.TAG_NAME, 'option')
+        options[2].click()
 
-    # Sum all the win rates for each player and divide by length of win_rates
-    avg_win_rate = sum(win_rates.values()) / len(win_rates)
+        # Get the page source
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
 
-    # Add player_name, and average win rate to a dictionary
-    average_win_rates[player_name] = avg_win_rate
+        # Find the win rate data
+        rows = soup.find_all('div', class_='css-k9b79k')
+        for row in rows:
+            player_name = row.find('div', class_='css-k6n90a')
+            win_rate = row.find('div', class_='css-he2tnk')
+            if player_name and win_rate and player_name.text in players:
+                # Remove % sign from win rate and convert to float
+                win_rate_val = float(win_rate.text.replace('%', ''))
+                # Add win rate to dictionary for player
+                if player_name.text in win_rates:
+                    win_rates[player_name.text] += win_rate_val
+                else:
+                    win_rates[player_name.text] = win_rate_val
+
+        player_name = soup.find('div', class_='css-1ob8mn4').text
+        player_records[player_name] = win_rates
+
+        overall_win_rates[player_name] = float(soup.find('div', class_='css-he2tnk').text.replace('%', ''))
+
+        # Sum all the win rates for each player and divide by length of win_rates
+        avg_win_rate = sum(win_rates.values()) / len(win_rates)
+
+        # Add player_name, and average win rate to a dictionary
+        average_win_rates[player_name] = avg_win_rate
+
+    # write player records, average_win_rates, and overall_win_rates to file
+    with open('player_records.txt', 'w') as f:
+        f.write(str(player_records))
+    with open('average_win_rates.txt', 'w') as f:
+        f.write(str(average_win_rates))
+    with open('overall_win_rates.txt', 'w') as f:
+        f.write(str(overall_win_rates))
+else:
+    # read player records, average_win_rates, and overall_win_rates from file
+    with open('player_records.txt', 'r') as f:
+        player_records = eval(f.read())
+    with open('average_win_rates.txt', 'r') as f:
+        average_win_rates = eval(f.read())
+    with open('overall_win_rates.txt', 'r') as f:
+        overall_win_rates = eval(f.read())
 
 iteration = 0
+initial_player_record = copy.deepcopy(player_records)
 while True:
-    print("\n>>> Iteration: " + str(iteration) + " <<<")
-    player_standing = get_curr_seeding(average_win_rates)
+    player_records = copy.deepcopy(initial_player_record)
 
-    for win_rates in player_records.values():
+    print("\n>>> Iteration: " + str(iteration) + " <<<")
+    if iteration == 0:
+        player_standing = get_curr_seeding(overall_win_rates)
+    else:
+        player_standing = get_curr_seeding(average_win_rates)
+
+    sum_of_weights = {}
+
+    for (curr_player, win_rates) in player_records.items():
+
         for competitor in win_rates.keys():
             player_index = player_standing.index(competitor) + 1
 
             # Assign win value based on player index + 1
-            # 1st-8th: 4 points,
-            # 9th-16th: 3 points,
-            # 17th-24nd: 2 points,
-            # 25th-32nd: 1 point
-            if player_index < 8:
-                win_value = 4
+            if player_index < 4:
+                win_value = 13
+            elif player_index < 8:
+                win_value = 11
+            elif player_index < 12:
+                win_value = 7
             elif player_index < 16:
-                win_value = 3
+                win_value = 4
             elif player_index < 24:
                 win_value = 2
             else:
@@ -113,19 +172,26 @@ while True:
 
             # Multiply win rate by win value
             win_rates[competitor] *= win_value
+            
+            # Add win_value to sum_of_weights
+            if curr_player in sum_of_weights:
+                sum_of_weights[curr_player] += win_value
+            else:
+                sum_of_weights[curr_player] = win_value
 
     # Calculate the average win rate for each player
     for player, win_rates in player_records.items():
-        # Sum all the win rates for each player and divide by length of win_rates
-        avg_win_rate = sum(win_rates.values()) / len(win_rates)
+        # Sum all the win rates for each player and divide by sum of weights (i.e., weighed average)
+        avg_win_rate = (sum(win_rates.values()) + len(win_rates)) / ( sum_of_weights[player])
         # Update the average win rate for the player
         average_win_rates[player] = avg_win_rate
     
     next_player_standing = get_curr_seeding(average_win_rates, False)
-    if player_standing == next_player_standing:
+    if is_seeding_stable(player_standing, next_player_standing, 1):
         break
     else:
         iteration += 1
 
-# Close the webdriver
-driver.close()
+if scrape:
+    # Close the webdriver
+    driver.close()
